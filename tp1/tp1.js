78a3764ccb2f5ace4c568f253c66d2db7ba3ca69
franclerc <104015573+franclerc@users.noreply.github.com>
2024-06-12 17:28:55 -0300

    Update tp1.js

--- tp1/tp1.js
+++ tp1/tp1.js
@@ -1,11 +1,27 @@
+let soundModelURL = 'https://teachablemachine.withgoogle.com/models/j6q0vREkM/';
+let mic;
 let prototipo;
+
+
+
+function preload() {
+  // Cargar el modelo de sonido
+  classifier = ml5.soundClassifier(soundModelURL + 'model.json');
+}
+
 
 function setup() {
   createCanvas(300,300);// Tamaño inicial del lienzo
   prototipo = new Estados();
+  
+   // Inicializar el micrófono
+  mic = new p5.AudioIn();
+  mic.start();
+  
+   // Clasificar el sonido cada vez que haya un resultado
+  classifier.classify(gotResult);
 
 }
-
 
 function draw() {
   background(0);
@@ -13,14 +29,34 @@ function draw() {
 }
 
 
-function keyPressed() {
-  if (key == 'p') {
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  
+  let label = results[0].label; 
+
+  if (label === 'Frenar') { // 
     prototipo.frenar();
-  }else if (key == 'a') {
+  } else if (label === 'Cambio') { //
     prototipo.cambiarCoordenadas();
-  }else if (key == 'r') { // Tecla para reiniciar
+  } else if (label === 'Reiniciar') { // 
     prototipo.reiniciar();
   }
 }
+
+
+//function keyPressed() {
+//  if (key == 'p') {
+//    prototipo.frenar();
+//  }else if (key == 'a') {
+//    prototipo.cambiarCoordenadas();
+//  }else if (key == 'r') { // Tecla para reiniciar
+//    prototipo.reiniciar();
+//  }
+//}
+
+
 
 
