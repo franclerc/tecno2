fabfb013f1d8149bcc9da6c910d1788da247b6f7
franclerc <104015573+franclerc@users.noreply.github.com>
2024-07-04 16:40:07 -0300

    Add files via upload

--- tp1/Estados.js
+++ tp1/Estados.js
@@ -1,100 +1,190 @@
 class Estados {
   constructor() {
+    this.filas = 0;
+    this.columnas = 0;
+    this.tamCelda = 75;
+
+    this.dimensiones = [
+       { // Canvas 1: 400x400
+         inicioX:25,
+        finalX: 410,
+        inicioY: 20,
+        finalY:410
+      },
+      
+      { // Canvas 2: 250x600
+        inicioX: 30,
+        finalX: 310,
+        inicioY: 30,
+        finalY: 627
+      },
+        { // Canvas 3: 300x500
+        inicioX: 25,
+        finalX: 330,
+        inicioY: 35,
+        finalY: 540
+      },
+         { // Canvas 4: 600x600
+        inicioX: 50,
+        finalX: 640,
+        inicioY: 50,
+        finalY: 640
+      },
+      
+      
+      
+        { // Canvas 1: 400x400
+         inicioX:25,
+        finalX: 410,
+        inicioY: 20,
+        finalY:410
+      },
+      
+      { // Canvas 2: 250x600
+        inicioX: 30,
+        finalX: 310,
+        inicioY: 30,
+        finalY: 627
+      },
+        { // Canvas 3: 300x500
+        inicioX: 25,
+        finalX: 330,
+        inicioY: 35,
+        finalY: 540
+      },
+         { // Canvas 4: 600x600
+        inicioX: 50,
+        finalX: 640,
+        inicioY: 50,
+        finalY: 640
+      },
+      
+      
+      
+      { // Canvas 1: 400x400
+         inicioX:25,
+        finalX: 410,
+        inicioY: 20,
+        finalY:410
+      },
+      
+      { // Canvas 2: 250x600
+        inicioX: 30,
+        finalX: 310,
+        inicioY: 30,
+        finalY: 627
+      },
+        { // Canvas 3: 300x500
+        inicioX: 25,
+        finalX: 330,
+        inicioY: 35,
+        finalY: 540
+      },
+         { // Canvas 4: 600x600
+        inicioX: 50,
+        finalX: 640,
+        inicioY: 50,
+        finalY: 640
+      },
+      
+      
+      
+       { // Canvas 1: 400x400
+         inicioX:25,
+        finalX: 410,
+        inicioY: 20,
+        finalY:410
+      },
+      
+      { // Canvas 2: 250x600
+        inicioX: 30,
+        finalX: 310,
+        inicioY: 30,
+        finalY: 627
+      },
+        { // Canvas 3: 300x500
+        inicioX: 25,
+        finalX: 330,
+        inicioY: 35,
+        finalY: 540
+      },
+         { // Canvas 4: 600x600
+        inicioX: 50,
+        finalX: 640,
+        inicioY: 50,
+        finalY: 640
+      },
+      
+       
+    
+    ];
+
     this.tiempo = 0;
     this.estado = 0;
-    this.frenoActivado = false;
+    this.frenoActivado= false;
     this.indiceMancha = 0;
     this.manchaCoordenadas = [];
 
-    this.tamanio = [  // tam de los canvas
-      { ancho: 400, alto: 400 },
-      { ancho: 400, alto: 400 },
-      { ancho: 400, alto: 400 },
-      { ancho: 400, alto: 400 },
+    this.tamanio = [ // tam de los canvas
       
+      
+       { ancho: 400, alto: 400 },
+        { ancho: 270, alto: 600 },
+        { ancho: 320, alto: 500},
+       { ancho: 600, alto: 600 },
+       
+       { ancho: 400, alto: 400 },
+        { ancho: 270, alto: 600 },
+        { ancho: 320, alto: 500},
+       { ancho: 600, alto: 600 },
+       
+       { ancho: 400, alto: 400 },
         { ancho: 250, alto: 600 },
-      { ancho: 250, alto: 600 },
-      { ancho: 250, alto: 600 },
-      { ancho: 250, alto: 600 },
-      
-        { ancho: 300, alto: 500 },
-      { ancho: 300, alto: 500 },
-      { ancho: 300, alto: 500 },
-      { ancho: 300, alto: 500 },
-      
-        { ancho: 600, alto: 600 },
-      { ancho: 600, alto: 600 },
-      { ancho: 600, alto: 600 },
-      { ancho: 600, alto: 600 },
+        { ancho: 320, alto: 500},
+       { ancho: 600, alto: 600 },
+       
+       { ancho: 400, alto: 400 },
+        { ancho: 270, alto: 600 },
+        { ancho: 320, alto: 500},
+       { ancho: 600, alto: 600 },
     ];
-    
 
-    this.colores = [  // fondos
+   
+    
+        this.colores = [ // fondos
       'data/Amarillo1.png',
+      'data/Rojo2.png',
+      'data/Rosa3.png',
+      'data/Verde4.png',
+      
       'data/Rojo1.png',
+      'data/Rosa2.png',
+      'data/Verde3.png',
+       'data/Amarillo4.png',
+       
+       
       'data/Rosa1.png',
+       'data/Verde2.png',
+       'data/Amarillo3.png',
+       'data/Rojo4.png',
+       
       'data/Verde1.png',
       'data/Amarillo2.png',
-      'data/Rojo2.png',
-      'data/Rosa2.png',
-      'data/Verde2.png',
-      'data/Amarillo3.png',
       'data/Rojo3.png',
-      'data/Rosa3.png',
-      'data/Verde3.png',
-      'data/Amarillo4.png',
-      'data/Rojo4.png',
       'data/Rosa4.png',
-      'data/Verde4.png'
+     
     ];
-
-    // Coordenadas específicas para cada tamaño de canvas
-    this.coordenadasPorTamanio = {
-      "400x400": [
-        { x: 30, y: 10 },  // Mancha 1
-        { x: 40, y: 80 },  // Mancha 2
-        { x: 100, y: 30 }, // Mancha 3
-        { x: 200, y: 70 },   // Mancha 4
-        { x: 20, y: 20 }, // Mancha 5
-        { x: 40, y: 10 }, // Mancha 6
-        { x: 100, y: 20 }, // Mancha 7
-        { x: 100, y: 0 }  // Mancha 8
-      ],
-      "250x600": [
-        { x: 50, y: 10 },
-        { x: 70, y: 20 },
-        { x: 100, y: 30 },
-        { x: 50, y: 40 },
-        { x: 20, y: 50 },
-        { x: 22, y: 150 },
-        { x: 50, y: -50 },
-        { x: 10, y: -100 }
-      ],
-      "300x500": [
-        { x: -40, y: 75 },
-        { x: -150, y: 100 },
-        { x: 15, y: 40 },
-        { x: 50, y: 10 },
-        { x: 10, y: 30 },
-        { x: -80, y: 30 },
-        { x: 60, y: 20},
-        { x: -50, y: -100 }
-      ],
-      "600x600": [
-        { x: -40, y: 10 },  
-        { x: 240, y: 160 },  
-        { x: 130, y: 140 },   
-        { x: 100, y: 100 }, 
-        { x: 200, y: 200 }, 
-        { x: 70  , y: -40 }, 
-        { x: 20, y: 0 },   
-        { x: 150, y: -180 }    
-      ]
-    };
 
     this.pintura = new Pintura();
     this.cargarImagenes();
     this.pintura.cargarImagenes();
+
+    this.crearGrilla();
+    
+     this.factorTam = 47; // tamaño pngs
+     this.achicar = true;
+     this.agrandar = false;
+     
   }
 
   cargarImagenes() {
@@ -106,6 +196,7 @@ class Estados {
   dibujar() {
     this.tiempo++;
     this.dibujartamanio();
+
     if (this.tiempo >= 100 && !this.frenoActivado) {
       this.estado = (this.estado + 1) % this.tamanio.length;
       this.tiempo = 0;
@@ -115,44 +206,102 @@ class Estados {
       image(this.colores[this.estado], 0, 0, this.tamanio[this.estado].ancho, this.tamanio[this.estado].alto);
     }
 
-    for (let i = 0; i < this.manchaCoordenadas.length; i++) {
-      let mancha = this.manchaCoordenadas[i];
-      image(mancha.imagen, mancha.x, mancha.y);
+
+
+  for (let i = 0; i < this.columnas; i++) {
+  for (let j = 0; j < this.filas; j++) {
+    let indiceImagen = i * this.filas + j;
+    if (indiceImagen < this.pintura.imagenes.length) {
+      let imagen = this.pintura.imagenes[indiceImagen];
+      image(imagen, this.grilla[i][j].x, this.grilla[i][j].y, this.factorTam, this.factorTam); // Tamaño arbitrario de la imagen
     }
   }
-  
+}
+
+
+ //this.cambiarTam();
+  }
+
+   crearGrilla() { // cantidad de columnas y filas
+    const canvasActual = this.dimensiones[this.estado];
+    this.columnas = int((canvasActual.finalX - canvasActual.inicioX) / this.tamCelda);
+    this.filas = int((canvasActual.finalY - canvasActual.inicioY) / this.tamCelda);
+
+    this.grilla = []; // Array para almacenar las coordenadas de cada celda
+
+    // Llenar la grilla con las coordenadas de cada celda
+    for (let i = 0; i < this.columnas; i++) {
+      this.grilla[i] = [];
+      for (let j = 0; j < this.filas; j++) {
+        this.grilla[i][j] = { x: canvasActual.inicioX + i * this.tamCelda, y: canvasActual.inicioY + j * this.tamCelda };
+        let factor= 70; // con 70 queda bien...
+        this.grilla[i][j].x += noise (i * this.filas+j,0) * factor - factor*0.5;
+         this.grilla[i][j].y += noise (0, i * this.filas+j) * factor - factor*0.5;
+      }
+    }
+  }
 
   dibujartamanio() {
     resizeCanvas(this.tamanio[this.estado].ancho, this.tamanio[this.estado].alto);
+    this.crearGrilla();
   }
-  
 
-  frenar() { // detener el tiempo en un canva determinado
+  frenar() { // detener el tiempo en un canvas determinado
     this.frenoActivado = true;
     this.tiempo = 0;
   }
 
-  agregarMancha() {  
-    if (this.frenoActivado && this.indiceMancha < this.pintura.imagenes.length) {
-      let imagenAleatoria = this.pintura.imagenes[this.indiceMancha];
-      let key = `${this.tamanio[this.estado].ancho}x${this.tamanio[this.estado].alto}`;
-      let coords = this.coordenadasPorTamanio[key][this.indiceMancha % this.coordenadasPorTamanio[key].length];
 
-      this.manchaCoordenadas.push({ imagen: imagenAleatoria, x: coords.x, y: coords.y });
-      this.indiceMancha++;
+  cambiarCoordenadas() {
+    // Mezclar aleatoriamente las imágenes en la lista
+    shuffle(this.pintura.imagenes, true);
+
+    // Redibujar la grilla con las imágenes mezcladas
+    for (let i = 0; i < this.columnas; i++) {
+      for (let j = 0; j < this.filas; j++) {
+        let indiceImagen = i * this.filas + j;
+        if (indiceImagen < this.pintura.imagenes.length) {
+          let imagen = this.pintura.imagenes[indiceImagen];
+          image(imagen, this.grilla[i][j].x, this.grilla[i][j].y, this.factorTam, this.factorTam); // Tamaño arbitrario de la imagen
+        }
+ 
+      }
     }
+    
   }
+  
+//ajustarTamanoYCambiarCoordenadas() {
+//  if (this.achicar) {
+//    if (this.factorTam > 10) {
+//      // Reducir el tamaño
+//      this.factorTam -= 10;
+//    } else {
+//      // Cambiar coordenadas cuando el tamaño mínimo se alcanza
+//      this.cambiarCoordenadas();
+//      // Restablecer la bandera para agrandar
+//      this.achicar = false;
+//      this.agrandar = true;
+//    }
+//  } else if (this.agrandar) {
+//    if (this.factorTam < 50) {
+//      // Aumentar el tamaño de vuelta al original
+//      this.factorTam += 10;
+//    } else {
+//      // Restablecer la bandera para el próximo ciclo de achicar
+//      this.achicar = true;
+//      this.agrandar = false;
+//    }
+//  }
+//}
+  
 
-  cambiarCoordenadas() { // cambiar las coordenadas de las manchas con letra a
-    let key = `${this.tamanio[this.estado].ancho}x${this.tamanio[this.estado].alto}`;
-    let coords = this.coordenadasPorTamanio[key];
-    let nuevaCoordenada = coords.shift(); // Extrae la primera coordenada del array
-    coords.push(nuevaCoordenada); // Añade la primera coordenada al final del array
 
-    // Actualiza las coordenadas de las manchas con las nuevas coordenadas
-    this.manchaCoordenadas.forEach((mancha, index) => {
-      mancha.x = coords[index].x;
-      mancha.y = coords[index].y;
-    });
+
+
+  reiniciar(){
+     this.frenoActivado = false;
+     this.estado=0;
+     this.tiempo = 0;
+     this.factorTam = 45;
   }
 }
