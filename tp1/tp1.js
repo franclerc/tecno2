6cf3dec6fc550d7f2d99fdfd71a20b3fc14a9432
franclerc <104015573+franclerc@users.noreply.github.com>
2024-07-10 15:00:06 -0300

    Add files via upload

--- tp1/tp1.js
+++ tp1/tp1.js
@@ -1,27 +1,69 @@
-let prototipo;
+let classifier;
+let soundModel = 'https://teachablemachine.withgoogle.com/models/RMENQs2CP/'; // nuevo
+let mic; 
+let tpfinal;
+let label = 'listening...';
+let vol;
+
+
+
+function preload() {
+ classifier = ml5.soundClassifier(soundModel + 'model.json');
+}
 
 function setup() {
-  createCanvas(300,300);// Tamaño inicial del lienzo
-  prototipo = new Estados();
+  createCanvas(300, 300); // Tamaño inicial del lienzo
+  tpfinal = new Estados();
+  
+  startListening();
+  userStartAudio(); 
+  classifier.classify(gotResult);
 
 }
 
+function startListening() {
+  
+  mic = new p5.AudioIn();
+  mic.start(acceso); 
+}
+
+function acceso() {
+  console.log('Micrófono listo');
+  
+ 
+}
 
 function draw() {
   background(0);
-  prototipo.dibujar();
+  tpfinal.dibujar();
+  vol = mic.getLevel();
+  console.log(vol);
+  
 }
 
-
-function keyPressed() {
-  if (key == 'p') {
-    prototipo.frenar();
-  }else if (key == 'a') {
-    prototipo.cambiarCoordenadas();
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
   }
-}
 
+  label = results[0].label;
+  
+  
 
-function mousePressed() {
-  prototipo.agregarMancha();
+  if (label === 'Frenar' && vol > 0.01 ) {
+    tpfinal.frenar();
+    tpfinal.desactivarTemblar();
+  } else if (label === 'Cambiar' &&  vol > 0.01) {
+    tpfinal.desactivarTemblar();
+    tpfinal.cambiarCoordenadas();
+    tpfinal.desactivarTemblar();
+  }else if (label === 'Vibrar' &&  vol >  0.001) {
+    tpfinal.activarTemblar();
+  }else if (label === 'Reiniciar' && vol > 0.0001) {
+    tpfinal.reiniciar();
+    tpfinal.desactivarTemblar();
+  }
+  
+   
 }
